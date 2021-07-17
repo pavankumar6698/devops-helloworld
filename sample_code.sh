c0ce13009f13387305c4c306fb6b9c1d529ae138
pavankumar6698 <87376255+pavankumar6698@users.noreply.github.com>
2021-07-17 19:25:56 +0530

    1st commit

--- sample_code.sh
+++ sample_code.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
 
-echo "This is an Samples Script from GitHub"
+echo "This is "
 echo "This is text from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
-echo "this is pavan "
